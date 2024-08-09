261afaa7f0623e00c60bfee93b86cc7e8650785d
SkretasLab <154230269+SkretasLab@users.noreply.github.com>
2024-08-09 17:30:56 +0300

    Delete tests directory

--- tests/seek_tax_results_analysis/analyze_seek_tax_results.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-python ps_caal_analysis.py -p /mnt/4529bb0c-30cc-4e67-8f04-e94a1b226730/Works/Enzymes_Metagenomes/results_seek_tax_eval -b proteins_blastp_nr.tsv -o analysis_results
