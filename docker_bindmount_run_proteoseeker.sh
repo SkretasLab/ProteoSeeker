0b7d9cc4ed329510803e5ecfe345d14bf42cd32f
SkretasLab <154230269+SkretasLab@users.noreply.github.com>
2024-08-22 14:44:53 +0300

    Add files via upload

--- docker_bindmount_run_proteoseeker.sh
+++ docker_bindmount_run_proteoseeker.sh
@@ -8,19 +8,19 @@ BIND_DIR="${HOME}/proteoseeker_bindmount"
 read -p "Select which example will be run.\nChoose to run ProteoSeeker based on template file by selecting a number from 1, 2, 3, 4, 5, 6 and 7: " nr_selection
 # Determine which example will be run.
 if [ $nr_selection = "1" ]; then
-	sudo docker run -i -t --mount type=bind,src="${BIND_DIR}",target=/home/ps_data proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_seek_tax_k_p.txt"
+	sudo docker run -i -t --mount type=bind,src="${BIND_DIR}",target=/home/ps_data skretaslab/proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_seek_tax_k_p.txt"
 elif [ $nr_selection = "2" ]; then
-	sudo docker run -i -t --mount type=bind,src="${BIND_DIR}",target=/home/ps_data proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_seek_tax_m_p.txt"
+	sudo docker run -i -t --mount type=bind,src="${BIND_DIR}",target=/home/ps_data skretaslab/proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_seek_tax_m_p.txt"
 elif [ $nr_selection = "3" ]; then
-	sudo docker run -i -t --mount type=bind,src="${BIND_DIR}",target=/home/ps_data proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_seek_tax_c_p.txt"
+	sudo docker run -i -t --mount type=bind,src="${BIND_DIR}",target=/home/ps_data skretaslab/proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_seek_tax_c_p.txt"
 elif [ $nr_selection = "4" ]; then
-	sudo docker run -i -t --mount type=bind,src="${BIND_DIR}",target=/home/ps_data proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_seek_p.txt"
+	sudo docker run -i -t --mount type=bind,src="${BIND_DIR}",target=/home/ps_data skretaslab/proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_seek_p.txt"
 elif [ $nr_selection = "5" ]; then
-	sudo docker run -i -t --mount type=bind,src="${BIND_DIR}",target=/home/ps_data proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_tax_k_p.txt"
+	sudo docker run -i -t --mount type=bind,src="${BIND_DIR}",target=/home/ps_data skretaslab/proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_tax_k_p.txt"
 elif [ $nr_selection = "6" ]; then
-	sudo docker run -i -t --mount type=bind,src="${BIND_DIR}",target=/home/ps_data proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_tax_m_p.txt"
+	sudo docker run -i -t --mount type=bind,src="${BIND_DIR}",target=/home/ps_data skretaslab/proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_tax_m_p.txt"
 elif [ $nr_selection = "7" ]; then
-	sudo docker run -i -t --mount type=bind,src="${BIND_DIR}",target=/home/ps_data proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_tax_c_p.txt"
+	sudo docker run -i -t --mount type=bind,src="${BIND_DIR}",target=/home/ps_data skretaslab/proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_tax_c_p.txt"
 else
     echo "Improper selection. Exiting."
     exit 1
--- docker_vol_run_proteoseeker.sh
+++ docker_vol_run_proteoseeker.sh
@@ -8,19 +8,19 @@ PS_VOL_NAME="proteoseeker_vol"
 read -p "Select which example will be run.\nChoose to run ProteoSeeker based on template file by selecting a number from 1, 2, 3, 4, 5, 6 and 7: " nr_selection
 # Determine which example will be run.
 if [ $nr_selection = "1" ]; then
-	docker run -i -t --mount source="${PS_VOL_NAME}",target=/home/ps_data proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_seek_tax_k_p.txt"
+	docker run -i -t --mount source="${PS_VOL_NAME}",target=/home/ps_data skretaslab/proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_seek_tax_k_p.txt"
 elif [ $nr_selection = "2" ]; then
-	docker run -i -t --mount source="${PS_VOL_NAME}",target=/home/ps_data proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_seek_tax_m_p.txt"
+	docker run -i -t --mount source="${PS_VOL_NAME}",target=/home/ps_data skretaslab/proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_seek_tax_m_p.txt"
 elif [ $nr_selection = "3" ]; then
-	docker run -i -t --mount source="${PS_VOL_NAME}",target=/home/ps_data proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_seek_tax_c_p.txt"
+	docker run -i -t --mount source="${PS_VOL_NAME}",target=/home/ps_data skretaslab/proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_seek_tax_c_p.txt"
 elif [ $nr_selection = "4" ]; then
-	docker run -i -t --mount source="${PS_VOL_NAME}",target=/home/ps_data proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_seek_p.txt"
+	docker run -i -t --mount source="${PS_VOL_NAME}",target=/home/ps_data skretaslab/proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_seek_p.txt"
 elif [ $nr_selection = "5" ]; then
-	docker run -i -t --mount source="${PS_VOL_NAME}",target=/home/ps_data proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_tax_k_p.txt"
+	docker run -i -t --mount source="${PS_VOL_NAME}",target=/home/ps_data skretaslab/proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_tax_k_p.txt"
 elif [ $nr_selection = "6" ]; then
-	docker run -i -t --mount source="${PS_VOL_NAME}",target=/home/ps_data proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_tax_m_p.txt"
+	docker run -i -t --mount source="${PS_VOL_NAME}",target=/home/ps_data skretaslab/proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_tax_m_p.txt"
 elif [ $nr_selection = "7" ]; then
-	docker run -i -t --mount source="${PS_VOL_NAME}",target=/home/ps_data proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_tax_c_p.txt"
+	docker run -i -t --mount source="${PS_VOL_NAME}",target=/home/ps_data skretaslab/proteoseeker:main_v1.0.0 -pfp "/home/ps_data/parameter_files/par_tax_c_p.txt"
 else
     echo "Improper selection. Exiting."
     exit 1
