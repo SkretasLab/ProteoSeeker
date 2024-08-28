d76545a0911334c60fd78055b85b9c7471792b4e
SkretasLab <154230269+SkretasLab@users.noreply.github.com>
2024-08-28 11:12:37 +0300

    Add files via upload

--- tests/tax_results_analysis/analyze_tax_results.sh
+++ tests/tax_results_analysis/analyze_tax_results.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -e
 
+# Get the current directory
+TAX_ANALYSIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# Get the parent directory which should be the tests directory of ProteoSeeker.
+TESTS_DIR=$(dirname "${TAX_ANALYSIS_DIR}")
+# Get the parent directory which should be the main directory of ProteoSeeker.
+PS_DIR=$(dirname "${TAX_ANALYSIS_DIR}")
+# Set the path of the directory with the results from the taxonomy evaluation.
+TAX_RESULTS_DIR="${PS_DIR}/results_tax_eval"
+
 # Kraken2 all
-python ps_br_analysis.py -p /mnt/4529bb0c-30cc-4e67-8f04-e94a1b226730/Works/Enzymes_Metagenomes/results_tax_eval -o analysis_plots_kraken_all -m k8,k16,k72,k8_0c1,k16_0c1,k72_0c1,k8_1c0,k16_1c0,k72_1c0,k8_10,k16_10,k72_10,k8_100,k16_100,k72_100,k8_ng,k16_ng,k72_ng -t False
+python ps_br_analysis.py -p "${TAX_RESULTS_DIR}" -o analysis_plots_kraken_all -m k8,k16,k72,k8_0c1,k16_0c1,k72_0c1,k8_1c0,k16_1c0,k72_1c0,k8_10,k16_10,k72_10,k8_100,k16_100,k72_100,k8_ng,k16_ng,k72_ng -t False
 
 # Kraken2 selected results and COMEBIn - nr and MetaBinner - nr results analysis
 # Top method:
@@ -11,4 +20,4 @@ python ps_br_analysis.py -p /mnt/4529bb0c-30cc-4e67-8f04-e94a1b226730/Works/Enzy
 # kraken_72: Sensitivity
 # kraken_72_ng: L1 Norm
 # Selected methods: k8_100,k8_1c0,k72,k72_ng
-python ps_br_analysis.py -p /mnt/4529bb0c-30cc-4e67-8f04-e94a1b226730/Works/Enzymes_Metagenomes/results_tax_eval -o analysis_plots_general -m k8_100,k8_1c0,k72,k72_ng,cnr,mnr
+python ps_br_analysis.py -p "${TAX_RESULTS_DIR}" -o analysis_plots_general -m k8_100,k8_1c0,k72,k72_ng,cnr,mnr
